ec1024d4ce7ec52369e60a904d3c73b26edd1ea4
dekr1sh <krishsonigra2175@gmail.com>
2024-11-01 02:34:49 +0530

    Take use of mocking to test the 'attack' function

--- src/tests/player.test.js
+++ src/tests/player.test.js
@@ -1,11 +1,12 @@
 import { Player } from '../modules/player.js';
+import { jest } from '@jest/globals';
 
 describe('Player', () => {
     let player;
     let opponent;
 
     beforeEach(() => {
-        player = Player('real');       
+        player = Player('human');       
         opponent = Player('computer'); 
     });
 
@@ -14,13 +15,25 @@ describe('Player', () => {
         expect(opponent.isComputer).toBe(true);
     });
 
-    test('real player attacks with specified coordinates', () => {
-        const result = player.attack(opponent.gameboard, 1, 1);
-        expect(result).toBeDefined();
+    test('human player attacks with specified coordinates', () => {
+        const mockReceiveAttack = jest.fn();
+        opponent.gameboard.receiveAttack = mockReceiveAttack;
+
+        player.attack(opponent.gameboard, 3, 4);
+        expect(mockReceiveAttack).toHaveBeenCalledWith(3, 4);
     });
 
     test('computer player attacks with random coordinates', () => {
-        const result = opponent.attack(player.gameboard);
-        expect(result).toBeDefined();
+        const mockReceiveAttack = jest.fn();
+        player.gameboard.receiveAttack = mockReceiveAttack;
+
+        opponent.attack(player.gameboard);
+        expect(mockReceiveAttack).toHaveBeenCalled();
+        const [x, y] = mockReceiveAttack.mock.calls[0];
+
+        expect(x).toBeGreaterThanOrEqual(0);
+        expect(x).toBeLessThan(10);
+        expect(y).toBeGreaterThanOrEqual(0);
+        expect(y).toBeLessThan(10);
     });
 });
